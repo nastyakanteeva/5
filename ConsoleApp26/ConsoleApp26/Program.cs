b45c0c7dbc60f8c342387450d76ac539672b105f
nastyakanteeva <117898518+nastyakanteeva@users.noreply.github.com>
2023-03-25 10:36:58 +0300

    Add files via upload

---
+++ ConsoleApp26/ConsoleApp26/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class Program
+{
+    static List<Product> products=new List<Product>();
+    static void Main(string[] args)
+    {
+        addProducts();
+        string name = Console.ReadLine();
+        foreach(Product product in products)
+        {
+            if(product.name == name)
+            {
+                Console.WriteLine(product.name);
+            }
+        }
+        int price = Convert.ToInt32(Console.ReadLine());
+        foreach(Product product in products)
+        {
+            if(product.price <= price)
+            {
+                Console.WriteLine(product.name);
+            }
+        }
+        int days = Convert.ToInt32(Console.ReadLine());
+        foreach(Product product in products)
+        {
+            if(product.days >= days)
+            {
+                Console.WriteLine(product.name);
+            }
+        }
+    }
+    static void addProducts()
+    {
+        products.Add(new Product("Колбаса","Черкизово",350,31,200));
+        products.Add(new Product("Молоко", "ЭкоНива", 100, 20, 387));
+        products.Add(new Product("Хлеб", "Хлебозовод", 50, 15, 450));
+    }
+    
+}
---
+++ ConsoleApp26/ConsoleApp26/product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Product
+{
+    public string name; //название
+    public string companyName; // производитель
+    public float price; //цена
+    public double days; // срок хранения
+    public int amt; // количество
+
+    public Product()
+{
+    name = "Unknown";
+    companyName = "Unknown";
+    price = 0;
+    days = 0;
+    amt = 0;
+       
+ }
+ public Product (string name, string companyName, float price, double days, int amt)
+    {
+        this.name = name; 
+        this.companyName = companyName;
+        this.price = price;
+        this.days = days;
+        this.amt = amt;
+    }
+    public void Print()
+    {
+        Console.WriteLine();
+    }
+
+}
